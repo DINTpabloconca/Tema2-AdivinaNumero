744de93f96279141b40db6c84a0dca8d7d780534
pabloconca <pabloconcasanchez@gmail.com>
2022-09-26 12:52:28 +0200

    Acertar el numero

--- Tema2-AdivinaNumero/MainWindow.xaml.cs
+++ Tema2-AdivinaNumero/MainWindow.xaml.cs
@@ -20,9 +20,42 @@ namespace Tema2_AdivinaNumero
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int random;
         public MainWindow()
         {
             InitializeComponent();
+            Random rnd = new Random();
+            random = rnd.Next(100 + 1);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           
+            int numeroIntroducido = Int32.Parse(numeroTextBox.Text);
+            if (numeroIntroducido == random)
+            {
+                resultadoTextBlock.Text = "Has acertado!";
+            }
+            else if (numeroIntroducido > random)
+            {
+                resultadoTextBlock.Text = "Te has pasado!";
+            }
+            else
+            {
+                resultadoTextBlock.Text = "Te has quedado corto!";
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Random rnd = new Random();
+            random = rnd.Next(100 + 1);
+            resultadoTextBlock.Text = "";
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
